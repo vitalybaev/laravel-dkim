f12f889c3bf13b2b1423f12a6e041d472d684aaa
henriklippke <info@henriklippke.de>
2017-08-14 10:35:03 +0200

    Support sending mail via queue method

--- src/DkimMailServiceProvider.php
+++ src/DkimMailServiceProvider.php
@@ -22,6 +22,8 @@ class DkimMailServiceProvider extends MailServiceProvider
             $mailer = new Mailer(
                 $app['view'], $app['swift.mailer'], $app['events']
             );
+            
+            $this->setMailerDependencies($mailer, $app);
 
             // If a "from" address is set, we will set it on the mailer so that all mail
             // messages sent by the applications will utilize the same "from" address
