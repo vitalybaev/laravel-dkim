ba3d407b4293a9f41e20fc1a0f60779ed9e29221
Vitaly Baev <dj@vitalybaev.ru>
2017-08-24 15:21:47 +0300

    Fixed #4 - Allow DKIM signing with sendmail and log driver.

--- src/Mailer.php
+++ src/Mailer.php
@@ -17,7 +17,7 @@ class Mailer extends \Illuminate\Mail\Mailer
             $message->from($this->from['address'], $this->from['name']);
         }
 
-        if (config('mail.driver') == 'smtp') {
+        if (in_array(strtolower(config('mail.driver')), ['smtp', 'sendmail', 'log'])) {
             if (config('mail.dkim_private_key') && file_exists(config('mail.dkim_private_key'))) {
                 if (config('mail.dkim_selector') && config('mail.dkim_domain')) {
                     $message->attachDkim(config('mail.dkim_selector'), config('mail.dkim_domain'), file_get_contents(config('mail.dkim_private_key')));
