a4db0b588b86d306f30b517ee89f0b5d0f9c2e48
Richard Danklof <richard.danklof@medpets.nl>
2019-01-17 08:34:20 +0100

    Added passphrase for parameter for private key

--- src/Message.php
+++ src/Message.php
@@ -10,12 +10,13 @@ class Message extends \Illuminate\Mail\Message
      * @param $selector
      * @param $domain
      * @param $privateKey
+     * @param $passphrase
      *
      * @return $this
      */
-    public function attachDkim($selector, $domain, $privateKey)
+    public function attachDkim($selector, $domain, $privateKey, $passphrase = '')
     {
-        $signer = new Swift_Signers_DKIMSigner($privateKey, $domain, $selector);
+        $signer = new Swift_Signers_DKIMSigner($privateKey, $domain, $selector, $passphrase);
         $signer->setHashAlgorithm(config('mail.dkim_algo', 'rsa-sha256'));
         if (config('mail.dkim_identity')) {
             $signer->setSignerIdentity(config('mail.dkim_identity'));
